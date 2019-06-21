b92b80d8837d14489524bdb88952c86f311bdc99
AskDevOps.org <51539433+askdevopsorg@users.noreply.github.com>
2019-06-21 23:22:41 +0100

    Update c.php

--- scripts/c.php
+++ scripts/c.php
@@ -15,10 +15,30 @@
 # 
 # Credit: Internet
 # http://www.askdevops.org/
+# Update get_client_ip_env function # Thanks to wgergeus
 #
 $timeout = 300; // 5 minutes
 $time = time();
-$ip = $_SERVER["REMOTE_ADDR"];
+
+function get_client_ip_env() {
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+     return $ipaddress;
+}
+
+$ip = get_client_ip_env();
 
 if(!filter_var($ip, FILTER_VALIDATE_IP)) {echo '...';exit;} # exit if the IP is invalid
 
